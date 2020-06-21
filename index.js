724fea75855d6c64adcff340d926c40922e7bd8e
jsm28415 <jsm.28415@gmail.com>
2020-06-21 20:43:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const nemo = ['nemo'];
+
+function findNemo(array){
+  for (let i=0; i < array.length; i++ ){
+    if(array[i] === 'nemo'){
+      console.log('Found Nemo!@')
+    }
+  }
+}
+findNemo(nemo);
