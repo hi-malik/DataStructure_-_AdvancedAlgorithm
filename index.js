954df9bcfc5d8989f21891f79cd2ea48ed40f935
Himanshu Malik <jsm.28415@gmail.com>
2020-06-22 20:07:23 +0000

    Nemo!Code Time Checked

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const nemo = ['nemo'];
 
 function findNemo(array){
+  let t0 = performance.now();
   for (let i=0; i < array.length; i++ ){
     if(array[i] === 'nemo'){
       console.log('Found Nemo!@')
     }
   }
+  let t1 = performance.now();
+  console.log("The speed of testing is " + (t1 - t0) + " milliseconds")
 }
 findNemo(nemo);
